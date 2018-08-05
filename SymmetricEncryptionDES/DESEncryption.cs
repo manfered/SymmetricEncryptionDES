9874c480f11847fb6e01cb846809ecfb894e7a59
manfered <35757736+manfered@users.noreply.github.com>
2018-08-05 13:37:09 -0600

    Add project files.

---
+++ SymmetricEncryptionDES/DESEncryption.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace SymmetricEncryptionDES
+{
+    public class DESEncryption
+    {
+        public byte[] GenerateRandomNumbers(int length)
+        {
+            using (var randomNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                var randomNumber = new byte[length];
+                randomNumberGenerator.GetBytes(randomNumber);
+
+                return randomNumber;
+            }
+        }
+
+        public byte[] Encrypt(byte[] dataToEncrypt, byte[] key, byte[] iv)
+        {
+            using (var des = new DESCryptoServiceProvider())
+            {
+                des.Mode = CipherMode.CBC;
+                des.Padding = PaddingMode.PKCS7;
+
+                des.Key = key;
+                des.IV = iv;
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    var cryptoStream = new CryptoStream(memoryStream, des.CreateEncryptor(), CryptoStreamMode.Write);
+
+                    cryptoStream.Write(dataToEncrypt, 0, dataToEncrypt.Length);
+                    cryptoStream.FlushFinalBlock();
+
+                    return memoryStream.ToArray();
+                }
+            }
+        }
+
+        public byte[] Decrypt(byte[] dataToDycrpt, byte[] key, byte[] iv)
+        {
+            using (var des = new DESCryptoServiceProvider())
+            {
+                des.Mode = CipherMode.CBC;
+                des.Padding = PaddingMode.PKCS7;
+
+                des.Key = key;
+                des.IV = iv;
+
+                using (var memoryStream = new MemoryStream())
+                {
+                    var cryptoStream = new CryptoStream(memoryStream, des.CreateDecryptor(), CryptoStreamMode.Write);
+
+                    cryptoStream.Write(dataToDycrpt, 0, dataToDycrpt.Length);
+                    cryptoStream.FlushFinalBlock();
+
+                    return memoryStream.ToArray();
+                }
+            }
+        }
+
+
+    }
+}
---
+++ SymmetricEncryptionDES/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymmetricEncryptionDES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var des = new DESEncryption();
+            var key = des.GenerateRandomNumbers(8);
+            var iv = des.GenerateRandomNumbers(8);
+            const string message = "Message to encrypt";
+
+            var encrypted = des.Encrypt(Encoding.UTF8.GetBytes(message), key, iv);
+            var decrypted = des.Decrypt(encrypted, key, iv);
+
+            var decryptedMessage = Encoding.UTF8.GetString(decrypted);
+
+            Console.WriteLine("DES Encryption");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine($"Message text = {message}");
+            Console.WriteLine($"Encrypted text = {Convert.ToBase64String(encrypted)}");
+            Console.WriteLine($"Decrypted text = {decryptedMessage}");
+        }
+    }
+}
